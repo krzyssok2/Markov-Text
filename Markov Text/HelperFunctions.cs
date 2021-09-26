eeb816ded537fff007a7b513f963fd5ea6713236
krzyssok2 <sokolkrzys1999@gmail.com>
2021-09-26 17:34:30 +0300

    Add project files.

---
+++ Markov Text/HelperFunctions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MarkovText
+{
+    class HelperFunctions
+    {
+        public string ReadFile(string path)
+        {
+            return System.IO.File.ReadAllText(path).ToLower();
+        }
+
+        public string Filter(string str)
+        {
+            var charsToRemove = new List<char>()
+            {
+                '.', '?', '!', ',', ':', ':', '-','\r' ,'\n'
+            };
+
+            str = str.Replace("\r\n", " ");
+
+            foreach (char c in charsToRemove)
+            {
+                if (c == '\n'|| c=='\r') 
+                {
+                    str = str.Replace(c.ToString(), " ");
+                    continue;
+                }
+                str = str.Replace(c.ToString(), String.Empty);
+            }
+
+            return str;
+        }
+
+        public void ExecuteMarkovText(string[] words, int nGram)
+        {
+            if(nGram==0)
+            {
+                Console.WriteLine("Ngram can't more less than 1");
+                return;
+            }
+            if(words.Length <= nGram)
+            {
+                Console.WriteLine("Amount of words can't be less or equal to nGram");
+                return;
+            }
+
+            Random random = new((int)DateTime.UtcNow.Ticks);
+
+            while (true)
+            {
+                var randomInt = random.Next(0, words.Length - 1 - nGram);
+
+                for(int i=randomInt;i<randomInt+nGram;i++)
+                {
+                    Console.Write(words[i] + " ");
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Markov Text/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarkovText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = @"D:\Users\MasterUser\Desktop\Information theory\Ipsum.txt";
+            var nGram = 5;
+
+            Console.WriteLine("Ksystof Stanislav Sokolovski Prifs 18/5 Markov Text \n");
+
+            HelperFunctions functionService = new();
+
+            var readFile = functionService.ReadFile(file);
+
+            var filteredString = functionService.Filter(readFile);
+
+            var words = filteredString.Split(' ');
+
+            functionService.ExecuteMarkovText(words, nGram);
+        }        
+    }
+}
